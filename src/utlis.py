5957e7161b755dcba421de1db02592c674cba7a3
shukla vedant <shuklavedant40@gmail.com>
2026-02-28 03:16:54 +0530

    Model Trainer

--- src/utlis.py
+++ src/utlis.py
@@ -1,21 +1,41 @@
-## Here all common things will be mention here
 import os
 import sys
 
-import numpy as np
-import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
 
-        os.makedirs(dir_path,exist_ok=True)
-
-        with open(file_path,"wb") as file_obj:
+        with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, X_test, y_train, y_test, models):
+    try:
+        report = {}
+        model_items = list(models.items())
+
+        for model_name, model in model_items:
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
